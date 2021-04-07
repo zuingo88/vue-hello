a2a0efd876bb44f6e534c8cf9d570b9edeb4a830
valerio sgura <valeriosgura88@gmail.com>
2021-04-07 11:30:28 +0200

    flipImg v-if

--- js/script.js
+++ js/script.js
@@ -7,23 +7,29 @@ function initVue() {
     data: {
       msg: "Titolo",
       paragrafo: "",
-      immagine: "img/verdena20.png",
+      immagine1: "img/verdena20.png",
+      immagine2: "img/palletto.png",
+      firstImg: true,
     },
     methods: {
-      change: function () {
-        if (
-          this.msg == "Titolo" &&
-          this.immagine == "img/verdena20.png" &&
-          this.paragrafo == ""
-        ) {
-          this.msg = "Nuovo titolo!";
-          this.paragrafo = "Ta-daaaaa!";
-          this.immagine = "img/palletto.png";
-        } else {
-          this.msg = "Titolo";
-          this.paragrafo = "";
-          this.immagine = "img/verdena20.png";
-        }
+      //change: function () {
+      // if (
+      //   this.msg == "Titolo" &&
+      //   this.immagine == "img/verdena20.png" &&
+      //   this.paragrafo == ""
+      // ) {
+      //   this.msg = "Nuovo titolo!";
+      //   this.paragrafo = "Ta-daaaaa!";
+      //   this.immagine = "img/palletto.png";
+      // } else {
+      //   this.msg = "Titolo";
+      //   this.paragrafo = "";
+      //   this.immagine = "img/verdena20.png";
+      // }
+
+      flip: function () {
+        this.firstImg = !this.firstImg;
+
       },
     },
   });
